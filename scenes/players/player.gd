dd8521cd20c111be2c00ab07c200089dffaf67e3
Ehab Aijaz <clutch.10306@gmail.com>
2026-04-25 13:48:26 +0530

    Resources and a trail mechanic when dashing

--- scenes/players/player.gd
+++ scenes/players/player.gd
@@ -5,6 +5,7 @@ var move_dir : Vector2
 @export var dash_duration := 0.1
 @export var dash_speed_multi := 2.5
 @export var dash_cooldown := 3.0
+@onready var trail: Trail = %Trail
 
 
 @onready var dash_timer: Timer = $DashTimer
@@ -14,14 +15,12 @@ var move_dir : Vector2
 func _ready() -> void:
 	dash_timer.wait_time = dash_duration
 	dash_cooldown_timer.wait_time = dash_cooldown
-	
 var is_dashing := false
 var dash_available := true
 
 func _process(delta: float) -> void:
-	var speed := 500
 	move_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var current_velocity := move_dir * speed
+	var current_velocity := move_dir * stats.speed
 	if is_dashing:
 		current_velocity *= dash_speed_multi
 	position += current_velocity * delta
@@ -45,6 +44,7 @@ func update_rotation() -> void:
 		visuals.scale = Vector2(0.5,0.5)
 
 func start_dash():
+	trail.start_trail()
 	is_dashing = true
 	dash_timer.start()
 	visuals.modulate.a = 0.5

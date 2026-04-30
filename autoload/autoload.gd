f60816c36524c3363647569ff01d6b756fdbdd66
Ehab Aijaz <clutch.10306@gmail.com>
2026-04-30 20:33:06 +0530

    Added flash, blocked ability and bases of melee weapons

--- autoload/autoload.gd
+++ autoload/autoload.gd
@@ -1,3 +1,20 @@
 extends Node
 
+signal on_create_block_text(unit : Node2D)
+signal on_create_damage_text(unit, hitbox: HitboxComponent)
 var player : Player
+const FLOATING_TEXT_SCENE = preload("uid://d1q2xjpgijnkm")
+const FLASH_MATERIAL = preload("uid://cbn5jqdtxi5yw")
+
+enum UpgradeTier {
+	COMMON,
+	RARE,
+	EPIC,
+	LEGENDARY
+}
+
+func get_chance_success(chance: float)-> bool:
+	var random := randf_range(0,1.0)
+	if random < chance:
+		return true
+	return false
--- scenes/arena/arena.gd
+++ scenes/arena/arena.gd
@@ -3,5 +3,32 @@ class_name Arena
 
 @export var player : Player
 
+@export var normal_color : Color
+@export var blocked_color : Color
+@export var critical_color : Color
+@export var hp_color : Color
+
+
 func _ready() -> void:
 	Global.player = player
+	Global.on_create_block_text.connect(_on_create_block_text)
+	Global.on_create_damage_text.connect(_on_create_damage_text)
+
+
+func create_floating_text(unit :Node2D):
+	var instance := Global.FLOATING_TEXT_SCENE.instantiate() as FloatingText
+	get_tree().root.add_child(instance)
+	var random_pos := randf_range(0,TAU)
+	var spawn_pos := unit.global_position  + Vector2.RIGHT.rotated(random_pos)
+	instance.global_position = spawn_pos
+	return instance
+	
+
+func _on_create_block_text(unit :Node2D):
+	var text = create_floating_text(unit)
+	text.setup("Blocked!", blocked_color)
+	
+func _on_create_damage_text(unit :Node2D, hitbox: HitboxComponent):
+	var text = create_floating_text(unit)
+	var color := critical_color if hitbox.critical else normal_color
+	text.setup(str(hitbox.damage), color)
